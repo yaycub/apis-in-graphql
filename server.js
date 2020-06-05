8d7a1eafe20f08d424282fe1e5a64a5378cd25fa
Jacob Harrington <jharrington4@gmail.com>
2020-06-05 13:06:42 -0700

    added quote by character to futurama query

--- server.js
+++ server.js
@@ -1,25 +1,23 @@
 const express = require('express');
 const express_graphql = require('express-graphql');
-
+const PORT = process.env.PORT || 8888;
 
 //Schemas
-const futuramaSchema = require('./lib/models/Futurama');
+const schema = require('./lib/models/Schema');
 
 //Routes
-const { getAllFuturamaQuotes } = require('./lib/routes/futurama');
+const { getAllFuturamaQuotes, getQuoteByChar } = require('./lib/routes/futurama');
+
 
 const app = express();
 
-const PORT = process.env.PORT || 8888;
-
-
- 
 const root = {
-  allFuturamaQuotes: getAllFuturamaQuotes
+  allFuturamaQuotes: getAllFuturamaQuotes,
+  futuramaQuoteByChar: getQuoteByChar
 };
 
-app.use('/futurama', express_graphql({
-  schema: futuramaSchema,
+app.use('/graphql', express_graphql({
+  schema: schema,
   rootValue: root,
   graphiql: true
 }));

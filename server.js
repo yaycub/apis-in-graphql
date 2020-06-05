25482fbbc4c8b8b991c819f371dcc1e1134e92c3
Jacob Harrington <jharrington4@gmail.com>
2020-06-05 11:47:46 -0700

    refactored into modules

--- server.js
+++ server.js
@@ -1,29 +1,18 @@
 const express = require('express');
 const express_graphql = require('express-graphql');
-const request = require('superagent');
-const { buildSchema } = require('graphql');
+
+
+//Schemas
+const futuramaSchema = require('./lib/models/Futurama');
+
+//Routes
+const { getAllFuturamaQuotes } = require('./lib/routes/futurama');
 
 const app = express();
 
-const futuramaSchema = buildSchema(`
-    type Query {
-      allFuturamaQuotes: [FuturamaQuote]
-    },
-    type FuturamaQuote {
-      character: String
-      quote: String
-      image: String
-    }
-`);
-
 const PORT = process.env.PORT || 8888;
 
-const getAllFuturamaQuotes = async() => {
-  const { body } = await request.get('http://futuramaapi.herokuapp.com/api/quotes/200');
-  
 
-  return body;
-};
  
 const root = {
   allFuturamaQuotes: getAllFuturamaQuotes

9507e1172e6da3375cb4ed7e204ef0d1ea29a59c
Jacob Harrington <jharrington4@gmail.com>
2020-06-05 11:43:04 -0700

    getAllFuturamaQuotes working

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
-require('dotenv').config();
-require('./lib/utils/connect')();
+const express = require('express');
+const express_graphql = require('express-graphql');
+const request = require('superagent');
+const { buildSchema } = require('graphql');
 
-const app = require('./lib/app');
+const app = express();
 
-const PORT = process.env.PORT || 7890;
+const futuramaSchema = buildSchema(`
+    type Query {
+      allFuturamaQuotes: [FuturamaQuote]
+    },
+    type FuturamaQuote {
+      character: String
+      quote: String
+      image: String
+    }
+`);
+
+const PORT = process.env.PORT || 8888;
+
+const getAllFuturamaQuotes = async() => {
+  const { body } = await request.get('http://futuramaapi.herokuapp.com/api/quotes/200');
+  
+
+  return body;
+};
+ 
+const root = {
+  allFuturamaQuotes: getAllFuturamaQuotes
+};
+
+app.use('/futurama', express_graphql({
+  schema: futuramaSchema,
+  rootValue: root,
+  graphiql: true
+}));
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
